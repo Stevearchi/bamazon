76b1d4ad54b9ba2a5e3163dd1707cddbd48240c4
Steve Archibald <stevearchi21@gmail.com>
2019-07-02 18:59:59 -0600

    added function for prompting and verifing quantity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,93 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
+var cTable = require('console.table');
+
+var connection = mysql.createConnection({
+    port: 3307,
+    database: 'bamazondb',
+    host: 'localhost',
+    user: 'root',
+    password: 'root'
+})
+
+
+
+function displayItems() {
+    connection.query('SELECT item_id, product_name, price FROM products', function (err, data) {
+        if (err) throw err;
+        var tableData = Array.from(Object.create(data)); // why doesn't this create a deep copy of the data array??
+        console.log(data[1].price)
+        tableData.forEach(function (item) {
+            item.price = `$` + item.price.toFixed(2);
+        });
+        console.log(data[1].price)
+        console.table(tableData);
+
+        promptItem(data);
+    });
+
+}
+
+function promptItem(data) {
+  
+    inquirer.prompt([{
+        type: 'input',
+        message: "Which item would you like to buy? Please enter the item ID",
+        name: 'whichId'
+    }]).then(function (answers) {
+        var price;
+        var id = answers.whichId;
+
+        var isValid = false; // verify id entered is valid
+        for (var i = 0; i < data.length; i++) {
+            if (id == data[i].item_id) {
+                isValid = true;
+                price = data[i].price;
+            };
+        };
+
+        if (isValid) {
+            verifyQuant(price, id);
+        } else {
+            console.log(`Please enter a valid item_id`);
+            promptItem(data);
+        }
+    });
+}
+
+function verifyQuant(price, id) {
+
+    connection.query('SELECT * FROM products', function (err, data) {
+        inquirer.prompt([{
+            type: 'input',
+            message: "How many would you like to buy?",
+            name: 'quantPurchasing'
+        }]).then(function (answers) {
+            connection.query('SELECT * FROM products WHERE item_id=?',
+                [id],
+                function (err, data) {
+                    // verifies quantity asked for is less than or equal to quantity in database
+                    var quantPurch = parseInt(answers.quantPurchasing);
+                    var stockquant = data[0].stock_quantity;
+                    if (parseInt(quantPurch) <= stockquant) {
+                        connection.query('UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?;',
+                            [quantPurch, id],
+                            function (err) {
+                                if (err) throw err;
+                            });
+  
+                        var totalPrice = '$' + (price * answers.quantPurchasing).toFixed(2);
+                        console.log(`Your total due is ${totalPrice}\n`)
+                        displayItems();
+                    } else {
+                        console.log(`\nI'm sorry, we have insufficient stock.\n`)
+                        displayItems();
+                    }
+                });
+        });
+
+    });
+};
+
+console.log("Welcome to Bamazon!\n");
+displayItems();
--- query.sql
+++ query.sql
@@ -1,10 +1,10 @@
 -- this  code is to be run to create/overwrite the data base with 10 initial entries
 
-DROP DATABASE IF EXISTS bamazon;
+DROP DATABASE IF EXISTS bamazondb;
 
-CREATE DATABASE bamazon;
+CREATE DATABASE bamazondb;
 
-USE bamazon;
+USE bamazondb;
 
 CREATE TABLE products (
 item_id INT NOT NULL AUTO_INCREMENT ,  -- unique item ID
@@ -21,7 +21,7 @@ VALUES ('6pck white socks', 'Apparal', 9.99, 101),
 ('6pck fat tire', 'Grocery', 7.99, 244343),
 ('6pck coors light', 'Grocery', 4.99, 2),
 ('dancing shoes', 'Apparal', 79.99, 78),
-('989 air jordans', 'Apparal', 299.98, 1),
+('1989 air jordans', 'Apparal', 399.98, 1),
 ('mtg black lotus (alpha) bgs 9.5', 'Collectables', 349999.00, 1),
 ('1952 micky mantle rookie topps bgs 5.5', 'Collectables', 139512.00, 1),
 ('86-87 michael jordan rookie fleer bgs 9.0', 'Collectables', 4700.00, 2),
